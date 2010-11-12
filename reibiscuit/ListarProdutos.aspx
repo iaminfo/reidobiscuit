49acf0e1ab896da4b46f734e7d7cc399c319df2f
maxwelgm@gmail.com <maxwelgm@gmail.com>
2010-11-11 22:01:19 -0200

    imagens no produtos listagem

--- reibiscuit/ListarProdutos.aspx
+++ reibiscuit/ListarProdutos.aspx
@@ -3,7 +3,7 @@
 <asp:Content ID="contentHead" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="contentMain" ContentPlaceHolderID="main" runat="server">
-    <asp:GridView ID="gvProdutos" runat="server" AutoGenerateColumns="False" >
+    <asp:GridView ID="gvProdutos" runat="server" AutoGenerateColumns="False" OnRowDataBound="GvProdutosOnRowDataBound">
         <Columns>
             <asp:TemplateField HeaderText="IdProduto">
                 <ItemTemplate>
@@ -19,7 +19,23 @@
                 <ItemTemplate>
                     <asp:Label ID="lCategoria" runat="server" Text='<%# Bind("Descricao") %>'></asp:Label>
                 </ItemTemplate>                
-            </asp:TemplateField>            
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Descrição">
+                <ItemTemplate>
+                    <asp:Image ID="ibImagem" runat="server" Width="100px" />
+                    <asp:GridView ID="gvImagens" runat="server" AutoGenerateColumns="false">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Imagem">
+                            <ItemTemplate>
+                                <a id="aImagem" runat="server" class="preview">
+                                <asp:Image ID="ibImagem" runat="server" AlternateText='<%# Bind("Nome") %>' Width="10px" />                
+                                </a>
+                            </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </ItemTemplate>                
+            </asp:TemplateField>
         </Columns>
     </asp:GridView>
 </asp:Content>
--- reibiscuit/ListarProdutos.aspx.cs
+++ reibiscuit/ListarProdutos.aspx.cs
@@ -11,26 +11,72 @@ namespace reibiscuit
 {
     public partial class ListarProdutos : System.Web.UI.Page
     {
+        private int? _idCategoria;
+        private GridView gvImagens = new GridView();
+
+        protected int? IdCategoria
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(Request.QueryString["IdCategoria"]))
+                    _idCategoria = Convert.ToInt16(Request.QueryString["IdCategoria"]);
+                return _idCategoria;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                
                 CarregarProdutos();
             }
         }
 
         private void CarregarProdutos()
         {
-            IList<Produto> lista = NProduto.RecuperarProdutos();
+            IList<Produto> lista = NProduto.RecuperarProdutos(null, IdCategoria);
             if (lista.Count == 0)
             {
                 Mensagem.EscreverMensagem(Mensagem.TiposMensagem.Info, Master.DivMensagem, "Nenhum Produto encontrado.");
                 return;
             }
+            Session.Add("lista",lista);
             gvProdutos.DataSource = lista;
             gvProdutos.DataBind();
 
+        }
+
+
+        protected void GvProdutosOnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType != DataControlRowType.DataRow) return;
+
+            IList<Produto> lista = (IList<Produto>) Session["lista"];
+            Produto p = lista[e.Row.DataItemIndex];
+            //IList<Imagem> listaImagens = NImagem.RecuperarImagens(p.IdProduto);
+            //p.Imagems.AddRange(listaImagens);
+            Image i = (Image)e.Row.FindControl("ibImagem");
+
+            string sFileName = p.Imagems.First().Nome;
+            const string sFileDir = "../Imagem/produtos/";
+            i.ImageUrl = sFileDir + sFileName;
+            //aImagem.HRef = sFileDir + sFileName;
+            gvImagens = (GridView) e.Row.FindControl("gvImagens");
+            gvImagens.DataSource = p.Imagems.ToList();
+            gvImagens.RowDataBound += new GridViewRowEventHandler(gvImagens_RowDataBound);
+            gvImagens.DataBind();
+        }
+
+        void gvImagens_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType != DataControlRowType.DataRow) return;
+            IList<Imagem> lista = (IList<Imagem>) gvImagens.DataSource;
+            Image i = (Image)e.Row.FindControl("ibImagem");
+
+            string sFileName = i.AlternateText;
+            const string sFileDir = "../Imagem/produtos/";
+            i.ImageUrl = sFileDir + sFileName;
+            //aImagem.HRef = sFileDir + sFileName;
         }
     }
 }
